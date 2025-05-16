299893fd2a16a7663ccdbc83345f4b25e9cb7dec
fadialjohari0 <fadialjohari0@gmail.com>
2025-05-16 19:05:36 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Property Finder App',
@@ -16,7 +15,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <Analytics />
       </body>
     </html>
   )
